573c908ac964bc66a4f903ae3628b619f097a524
ranushka-lakmal <ranulakmal@gmail.com>
2023-03-01 01:15:25 +0530

    add properties

---
+++ src/main/java/com/example/employeecrud/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.employeecrud.controller;
+
+import com.example.employeecrud.model.Employee;
+import com.example.employeecrud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	//get all employee
+	@GetMapping("/employee")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+
+}
