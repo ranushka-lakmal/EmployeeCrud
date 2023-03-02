61a9d0a753fa4eb275c3a6be798639d1c6a241c3
ranushka-lakmal <ranulakmal@gmail.com>
2023-03-03 00:33:28 +0530

    add routing and navigation

--- Frontend/angular-employee-management-frontend/src/app/app.module.ts
+++ Frontend/angular-employee-management-frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {HttpClientModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
