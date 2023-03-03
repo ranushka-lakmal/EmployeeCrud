4fc086cac2498a04342cad39fd939c2af2052103
ranushka-lakmal <ranulakmal@gmail.com>
2023-03-04 00:43:15 +0530

    create employee REST API

--- Frontend/angular-employee-management-frontend/src/app/app.module.ts
+++ Frontend/angular-employee-management-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent
+    EmployeeListComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
